23ace6836de528131279369b3068ad38a4589cd8
JongHo <ehdus232323@naver.com>
2024-09-04 15:14:10 +0900

    로그인 작업중

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login'); 
+  }
+
   return (
     <header className="header">
       <div className="logo">9900</div>
       <input type="text" placeholder="검색" />
       <div className="login-buttons">
-        <button>Login</button>
+        <button onClick={handleLoginClick}>Login</button>
       </div>
     </header>
   );
