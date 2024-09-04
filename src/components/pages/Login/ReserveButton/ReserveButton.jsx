8c42f52f2b1af11ef8a17aac61cd5797da0abd18
LeeWonHee <leewh991106@gmail.com>
2024-09-05 00:18:57 +0900

    예약 페이지 모달 작업
    예약 페이지 예약 날짜, 인원 수, 시간 선택 ui 작업

--- src/components/pages/Login/ReserveButton/ReserveButton.jsx
+++ src/components/pages/Login/ReserveButton/ReserveButton.jsx
@@ -9,15 +9,69 @@ function ReserveModal() {
     const closeModal = () => {
         navigate(-1)
     }
-    asdsadsaddasdasdsd
     // 날짜 선택
     const [reserveDate, setReserveDate] = useState(new Date())
     // 인원 선택
-    const [numberOfGuests, setNuberOfGuests] = useState(1)
+    const [numberOfGuests, setNumberOfGuests] = useState(1)
+    // 시간 선택
+    const [reserveTime, setReserveTime] = useState('')
+
+    const handleDateChange = date => {
+        setReserveDate(date)
+    }
+
+    const handleGuestClick = guests => {
+        setNumberOfGuests(guests)
+    }
+
+    const handleTimeClick = time => {
+        setReserveTime(time)
+    }
+
+    cons
 
     return (
         <div className={Styles.reserveModalContainer}>
             <h1>오늘</h1>
+            <Calendar onChange={handleDateChange} value={reserveDate} />
+            <div className={Styles.guestButtons}>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(guests => {
+                    ;<div
+                        key={guests}
+                        className={`${Styles.guestButton} ${
+                            numberOfGuests === guests ? Styles.active : ''
+                        }`}
+                        onClick={() => handleGuestClick(guests)}
+                    >
+                        {guests}명
+                    </div>
+                })}
+            </div>
+            <div className={Styles.reserveTimeButtons}>
+                {[
+                    '오후 12:00',
+                    '오후 12:30',
+                    '오후 1:00',
+                    '오후 1:30',
+                    '오후 2:00',
+                    '오후 2:30',
+                    '오후 3:00',
+                    '오후 3:30',
+                ].map(time => (
+                    <div
+                        key={time}
+                        className={`${Styles.timeButton} ${
+                            reserveTime === time ? Styles.active : ''
+                        }`}
+                        onClick={() => handleTimeClick(time)}
+                    >
+                        {time}
+                    </div>
+                ))}
+            </div>
+            <button onClick={closeModal}>닫기</button>
         </div>
     )
 }
+
+export default ReserveModal
