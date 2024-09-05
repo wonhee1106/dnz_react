42c2c2df5e57014b25b30adbcbab9c20752bce25
JW <sinjawoo@naver.com>
2024-09-05 10:17:41 +0900

    main page fix3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // FontAwesomeIcon 컴포넌트
+import { faBell } from '@fortawesome/free-regular-svg-icons'; // faBell 아이콘 임포트
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'; // faBookmark 아이콘 임포트
 
 const Header = () => {
   const navigate = useNavigate();
@@ -11,10 +14,16 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="logo">9900</div>
-      <input type="text" placeholder="검색" />
-      <div className="login-buttons">
-        <button onClick={handleLoginClick}>Login</button>
+      <div className="header-section logo">9900</div>
+      <div className="header-section search-container">
+        <input className="search" type="text" placeholder="검색" />
+      </div>
+      <div className="header-section icon">
+        <FontAwesomeIcon icon={faBookmark} className="faBookmark" />
+        <FontAwesomeIcon icon={faBell} className="faBell" />
+      </div>
+      <div className="header-section login-buttons">
+        <button onClick={handleLoginClick}>로그인</button>
       </div>
     </header>
   );
