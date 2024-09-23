dcfe588dcb166e539de81b8e483fae22c3bc9c5a
JongHo <ehdus232323@naver.com>
2024-09-23 12:36:38 +0900

    커밋

--- src/components/InputGroup/index.jsx
+++ src/components/InputGroup/index.jsx
@@ -1,10 +1,18 @@
-import styles from "./index.module.css"
-import { useEffect, useState } from 'react';
+import styles from "./index.module.css";
+import { useState } from 'react';
 
-const InputGroup = ({ type, title, description, placeholder="", btnComment, click, keyUp, genderValue }) => {
-
-
+const InputGroup = ({ type, title, description, placeholder = "", btnComment, click, keyUp, genderValue, name }) => {
     const [gender, setGender] = useState('M');
+
+    const handleKeyUp = (e) => {
+        if (keyUp && e.target) {
+            keyUp({
+                name,
+                value: e.target.value,
+            });
+        }
+    };
+    
 
     return (
         <div className={styles.Container}>
@@ -14,21 +22,21 @@ const InputGroup = ({ type, title, description, placeholder="", btnComment, clic
             </div>
             <div className={styles.Content}>
                 {type !== "gender" ? 
-                    <input type={type} placeholder={placeholder} onKeyUp={(e)=> {keyUp(e.target.value);}} />
+                    <input type={type} placeholder={placeholder} onKeyUp={handleKeyUp} name={name} />
                     :
                     <>
-                        <label htmlFor="genderM" id={gender == "M" && styles.genderM} onClick={()=>{setGender("M"); genderValue("M");}}  className={styles.gender} >남</label>
-                        <label htmlFor="genderF" id={gender == "F" && styles.genderF} onClick={()=>{setGender("F"); genderValue("F");}}  className={styles.gender} >여</label>
-                        <input type="radio" id="genderM" name="gender" value="M" checked hidden  />
-                        <input type="radio" id="genderF" name="gender" value="F" hidden/>
+                        <label htmlFor="genderM" id={gender === "M" && styles.genderM} onClick={() => { setGender("M"); genderValue("M"); }} className={styles.gender}>남</label>
+                        <label htmlFor="genderF" id={gender === "F" && styles.genderF} onClick={() => { setGender("F"); genderValue("F"); }} className={styles.gender}>여</label>
+                        <input type="radio" id="genderM" name="gender" value="M" checked hidden />
+                        <input type="radio" id="genderF" name="gender" value="F" hidden />
                     </>
                 }
                 {btnComment && 
-                    <button onClick={()=>click()}>{btnComment}</button>
+                    <button onClick={() => click()}>{btnComment}</button>
                 }
             </div>
         </div>
     );
-}
+};
 
 export default InputGroup;
